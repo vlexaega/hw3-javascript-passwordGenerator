a8fc36e5b210b8a7277038d1e56a2ceba63014c3
vlexaega <132007323+vlexaega@users.noreply.github.com>
2023-05-17 20:26:46 -0400

    added wireframe to code

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 // Assignment code here
 
 
+
+
+// DEPENDENCIES
+    // user selecting box
+
+// DATA (globalvariables)
+    // password?
+    // alphabet
+    // numbers
+    // symbols 
+
+// FUNCTIONS
+    // generatePassword 
+    // return 8 letters of alphabet
+    // return 2 numbers 
+    // return 1 symbol
+
+// USER INTERACTIONS
+    // user clicks generate password 
+
+// INITIALIZATION
+    // function to generatePassword
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
